1fa5d7f9b32e08c50ae702e599739a9417a5db7c
merororo2 <108105625+merororo2@users.noreply.github.com>
2022-12-21 11:19:07 +0900

    Update-header

--- client/src/components/Home/Header/HomeHeader.jsx
+++ client/src/components/Home/Header/HomeHeader.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import Header from '../../common/Header/Header';
 import HeaderEventBanner from '../../common/Header/HeaderEventBanner';
-import HeaderHomeLogo from '../../common/Header/HeaderHomeLogo';
 function HomeHeader() {
-  const HeaderContainer = styled.header`
-    width: 100%;
-    display: flex;
-  `;
   return (
     <div>
       <HeaderEventBanner />
-      <HeaderContainer>
-        <HeaderHomeLogo />
-        <Header />
-      </HeaderContainer>
+      <Header />
     </div>
   );
 }
--- client/src/components/common/Header/Header.jsx
+++ client/src/components/common/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import HeaderMenu from './HeaderMenu';
+import HeaderHomeLogo from './HeaderHomeLogo';
 
 function Header() {
+  const HeaderContainer = styled.header`
+    width: 100%;
+    display: flex;
+  `;
   return (
     <div>
-      <HeaderMenu />
+      <HeaderContainer>
+        <HeaderHomeLogo />
+        <HeaderMenu />
+      </HeaderContainer>
     </div>
   );
 }
