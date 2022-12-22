3dd2b9dda41d68e69d90f2e2ba7f23f447facd12
merororo2 <108105625+merororo2@users.noreply.github.com>
2022-12-22 10:42:33 +0900

    Update-HeaderSearchForm

--- client/src/components/common/Header/HeaderSearchForm.jsx
+++ client/src/components/common/Header/HeaderSearchForm.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 function HeaderSearchForm() {
   return (
     <div>
-      <input type="text" className="w-64 h-8" />
+      <input type="text" className="w-64 h-8 bg-gray-200 ml-4" placeholder="상품을 입력하세요" />
       <Link to="/">
-        <i className="fa-solid fa-magnifying-glass fa-xl"></i>
+        <i className="fa-solid fa-magnifying-glass fa-xl ml-4"></i>
       </Link>
     </div>
   );
