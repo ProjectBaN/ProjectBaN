de530e9f860af06a3bf8d3eb6267fbdffcfd4925
merororo2 <sewh633@naver.com>
2022-12-24 11:45:18 +0900

    Create-EventBannerSlide

--- client/src/components/EventBanner/EventBannerSlide.jsx
+++ client/src/components/EventBanner/EventBannerSlide.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+
+import testImage1 from '../../asset/images/EventBannerSlide1.jpg';
+import testImage2 from '../../asset/images/EventBannerSlide2.jpg';
+import testImage3 from '../../asset/images/EventBannerSlide3.jpg';
+
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+
+SwiperCore.use([Navigation, Pagination]);
 
 function EventBannerSlide() {
-  return <div></div>;
+  return (
+    <div className="">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>
+          <img className="w-full" src={testImage1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="w-full" src={testImage2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="w-full" src={testImage3} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
 export default EventBannerSlide;
--- client/src/components/Home/Body/HomeBody.jsx
+++ client/src/components/Home/Body/HomeBody.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import EventBannerSlide from '../../EventBanner/EventBannerSlide';
 
 function HomeBody() {
-  return <div className="w-full">hi</div>;
+  return (
+    <div className="flex flex-col  justify-center  w-full h-full ">
+      <EventBannerSlide />
+    </div>
+  );
 }
 
 export default HomeBody;
