5cc8c339a147d5b31ad1e3c235e46373a011383a
merororo2 <sewh633@naver.com>
2022-12-26 15:40:12 +0900

    Update-Footer

--- client/src/components/common/Footer/FooterCustomerCenter.jsx
+++ client/src/components/common/Footer/FooterCustomerCenter.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function FooterCustomerCenter() {
   return (
     <div className="w-1/2 ">
-      <p className=" text-lg mb-PCsm font-bold ">고객센터</p>
-      <p className=" text-sm">TEL : 010-1234-5678</p>
-      <p className=" text-sm">운영시간 : 09:00 ~ 18:00 </p>
+      <p className="text-lg mb-PCsm font-bold ">고객센터</p>
+      <p className="text-sm">TEL : 010-1234-5678</p>
+      <p className="text-sm">운영시간 : 09:00 ~ 18:00 </p>
     </div>
   );
 }
