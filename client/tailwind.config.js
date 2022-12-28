969a65c25100fd05464d8d4565d1657670bfa06e
merororo2 <sewh633@naver.com>
2022-12-28 15:56:42 +0900

    Update-tailwind theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -35,8 +35,9 @@ module.exports = {
         hoverShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
       },
       width: {
-        pcContainer: '1200px',
-        pcSearchForm: '440px',
+        PcContainer: '1200px',
+        PcSearchForm: '440px',
+        MbSearchForm: '200px',
         signUpContainer: '480px',
       },
     },
