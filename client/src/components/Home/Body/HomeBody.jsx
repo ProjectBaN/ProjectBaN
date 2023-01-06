5548b4bdc723c682863b8aef61ead9a381c36839
merororo2 <sewh633@naver.com>
2023-01-07 00:03:26 +0900

    install-axios

--- client/src/components/Home/Body/HomeBody.jsx
+++ client/src/components/Home/Body/HomeBody.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import EventBannerContainer from '../../common/EventBannerContainer/EventBannerContainer';
 import BestProductContainer from '../Container/BestProductContainer';
 import QuickCategoryMenu from './QuickCategoryMenu';
 import RecommendProductContainer from './RecommendProductContainer';
 
 function HomeBody() {
+  useEffect(() => {}, []);
   return (
     <div className="mt-MbSm lg:mt-PcSm ">
       <EventBannerContainer />
